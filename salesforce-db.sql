58c3309bdd046534cb01e97e02e1354b48ef0c50
Dhamodharan889342 <77376906+Dhamodharan889342@users.noreply.github.com>
2021-01-26 16:45:16 +0530

    Update salesforce-db.sql

--- salesforce-db.sql
+++ salesforce-db.sql
@@ -1,4 +1,4 @@
-create table CustomLabel(
+create table salesforcecicd.CustomLabel(
 AfterSales_SaveAndNewProductLineItem varchar(50) PRIMARY KEY,
 AfterSales_SaveProductLineItem varchar(50) NOT NULL,
 AfterSales_SearchProductLineItem varchar(50) NOT NULL
