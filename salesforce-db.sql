f82d7090a0c54e5f1a6c530bb340fc8991c471c0
Dhamodharan889342 <77376906+Dhamodharan889342@users.noreply.github.com>
2021-01-26 15:59:17 +0530

    Create salesforce-db.sql

---
+++ salesforce-db.sql
@@ -0,0 +1,5 @@
+create table CustomLabel(
+AfterSales_SaveAndNewProductLineItem varchar(50) PRIMARY KEY,
+AfterSales_SaveProductLineItem varchar(50) NOT NULL,
+AfterSales_SearchProductLineItem varchar(50) NOT NULL
+);
